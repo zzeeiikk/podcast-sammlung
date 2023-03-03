a558182539fce08fd133e468958d1adda5b1f3d5
Timo Wulff <timo@Air-von-Timo.fritz.box>
2023-03-03 20:26:29 +0100

    add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-
-
-
-
+    <Footer />
 </template>
 
 <script>
+import Footer from './components/Footer.vue';
+
+export default {
+    name: 'app',
+    components: {
+        Footer,
+    }
+}
 
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <a href="http://www.timo-wulff.de">www.timo-wulff.de</a>
+                <br>
+                Powered by <a href="http://vuejs.org">Vue.js</a>, <a href="https://bulma.io">Bulma</a>, <a
+                    href="https://github.com/eazyliving/fyyd-api">Fyyd-Api</a> | All source code available on <a
+                    href="https://github.com/zzeeiikk/">Github</a>.
+                <br>
+                The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComp',
+}
+</script>
