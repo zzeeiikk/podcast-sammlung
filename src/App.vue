44d2ae476e1ea26bd9d91d7598bb0503dee3ff18
Timo Wulff <timo@Air-von-Timo.fritz.box>
2023-03-03 22:01:16 +0100

    add GET Request to fyyd, Add Cards for Podcasts

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
+    <PodcastList />
     <Footer />
 </template>
 
 <script>
+import PodcastList from './components/Podcasts/PodcastList.vue';
 import Footer from './components/Footer.vue';
 
 export default {
     name: 'app',
     components: {
+        PodcastList,
         Footer,
     }
 }
---
+++ src/components/Podcasts/PodcastList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <h1 class="title is-1">Podcast Sammlung</h1>
+
+
+
+      <!-- Card -->
+      <div class="columns is-multiline">
+        <div v-for="id in ids" :key="id" class="column is-half-tablet is-one-third-desktop is-one-quarter-fullhd">
+
+          <div class="card has-text-left bm--card-equal-height" @click="showModal">
+
+            <Card :id="id.id" :name="id.title" :daten="id" />
+
+          </div>
+
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
+</template>
+  
+  
+<script>
+import axios from "axios";
+import Card from "./Card.vue";
+
+export default {
+  name: "Podcast-List",
+  components: {
+    Card,
+  },
+
+  data() {
+    return {
+      ids: null,
+      showModalFlag: false,
+    };
+  },
+
+  created: function () {
+    axios
+      .get("id.json")
+      .then((res) => {
+        this.ids = res.data
+      })
+  },
+
+
+
+
+}
+</script>
+  
+<style scoped>
+.bm--card-equal-height {
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+  overflow: hidden;
+}
+
+.bm--card-equal-height .card-footer {
+  margin-top: auto;
+}
+</style>
