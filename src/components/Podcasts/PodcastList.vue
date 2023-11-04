c24c710e3653a83f83f8caa795e8e7e2ca86e061
zzeeiikk <zeik@systemli.org>
2023-11-04 12:16:46 +0100

    -

--- src/components/Podcasts/PodcastList.vue
+++ src/components/Podcasts/PodcastList.vue
@@ -3,19 +3,6 @@
     <div class="container">
       <h1 class="title is-1">Podcast Sammlung</h1>
 
-      <!-- TOP -->
-      <div class="columns is-multiline">
-        <div v-for="id in ids" :key="id" class="column is-half-tablet is-one-third-desktop is-one-quarter-fullhd">
-
-          <div class="card has-text-left bm--card-equal-height" @click="showModal">
-
-            <top :id="id.id" :name="id.title" :daten="id" />
-
-          </div>
-        </div>
-      </div>
-
-
       <!-- Card -->
       <div class="columns is-multiline">
         <div v-for="id in ids" :key="id" class="column is-half-tablet is-one-third-desktop is-one-quarter-fullhd">
@@ -27,12 +14,6 @@
           </div>
         </div>
       </div>
-
-
-
-
-
-
     </div>
   </div>
 </template>
@@ -41,13 +22,11 @@
 <script>
 import axios from "axios";
 import Card from "./Card.vue";
-import top from "./top.vue";
 
 export default {
   name: "Podcast-List",
   components: {
     Card,
-    top,
   },
 
   data() {
