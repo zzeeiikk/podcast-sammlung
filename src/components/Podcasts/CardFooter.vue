7c3727cfdcad8f2d063101d88af7a619a5f1aaee
zzeeiikk <zeik@systemli.org>
2023-11-04 12:07:06 +0100

    move footer to seperate file

---
+++ src/components/Podcasts/CardFooter.vue
@@ -0,0 +1,34 @@
+<template>
+    
+  <a v-for="tags in casts.categories" :key="tags" href="#" class="card-footer-item">{{ tags }}</a>
+
+</template>
+    
+    
+<script>
+import axios from "axios";
+
+export default {
+    name: "CardFooter",
+
+    data() {
+        return {
+            showModalFlag: false,
+        };
+    },
+
+    props: ["casts"],
+
+    categories: function () {
+    axios
+      .get("categories.json")
+      .then((res) => {
+        this.cat = res.data
+        console.log(this.cat)
+      }
+    )
+  },
+
+}
+</script>
+
